5c211fe69ee1485a1402e0cfa08876b930cfcc6a
iconicayansh-dev <iconicayansh@gmail.com>
2025-08-24 06:50:51 +0000

    testimonials

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const config = require('./config');
 const axios = require('axios');
 const { json } = require('body-parser');
 
@@ -17,49 +16,6 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
-app.get("/", (req, res) => {
-    // let repoInfo;
-    // axios({
-    //     method: "get",
-    //     url: `https://api.github.com/users/${config.githubUsername}/repos`,
-    //     headers: {
-    //         Authorization: `Bearer ${config.githubToken}`,
-    //         "Content-Type": "application/json",
-    //         "Accept": "application/vnd.github.mercy-preview+json" // MUST ADD TO INCLUDE TOPICS
-    //     }
-    // }).then(response => {
-    //    repoInfo = response.data;
-    //    res.render('index', {
-    //     repoInfo: repoInfo
-    // });
-    // })
-    // .catch(err => {
-    //     console.log(err);
-    // });
-    res.render('index')
-})
-
-app.get("/projects", (req, res) => {
-        let repoInfo;
-    axios({
-        method: "get",
-        url: `https://api.github.com/users/${config.githubUsername}/repos`,
-        headers: {
-            Authorization: `Bearer ${config.githubToken}`,
-            "Content-Type": "application/json",
-            "Accept": "application/vnd.github.mercy-preview+json" // MUST ADD TO INCLUDE TOPICS
-        }
-    }).then(response => {
-       repoInfo = response.data;
-       res.render('projects', {
-        repoInfo: repoInfo
-    });
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-
 app.listen(port, () => {
     console.log(`listening on port: ${port}`);
-})
+});
