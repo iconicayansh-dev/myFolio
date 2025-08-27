55735d288292f5e6391ff0bd671f7dd334b8f307
iconicayansh-dev <iconicayansh@gmail.com>
2025-08-27 06:15:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ const express = require('express');
 const path = require('path');
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const { json } = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cors());
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-res.render('index')
-})
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`listening on port: ${port}`);
-})
+});
