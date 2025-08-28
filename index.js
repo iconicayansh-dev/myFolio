e3dd9aaeacd179b419a41cbb6f2bc950a2129ac6
iconicayansh-dev <iconicayansh@gmail.com>
2025-08-28 17:02:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+    res.status(200).sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
 app.get("/cats-hub", (req, res) => {
   res.redirect("https://discord.gg/cats-hub-gg-freetag-945227072492691537");
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'pages', '404.html'));
 });
 
 app.listen(port, () => {
